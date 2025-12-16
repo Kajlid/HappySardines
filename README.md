ea503fb5d9137192156ac50796438e892c8da50a
Kajlid <104261817+Kajlid@users.noreply.github.com>
2025-12-16 13:45:03 +0100

    chore(deps): initialize environment and install packages

---
+++ README.md
@@ -0,0 +1,4 @@
+
+Environment:
+
+Run ```uv sync``` to update the environment with the correct dependencies. 
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from happysardines!")
+
+
+if __name__ == "__main__":
+    main()
